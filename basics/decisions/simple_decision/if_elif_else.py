c9eb0cd0b0daf03c0c6eead34cd6fa551cdfe7cb
Uthman Lawal <5lawau84@solent.ac.uk>
2021-10-08 15:56:21 +0100

    Added code to demonstrate the use of an if...elif...else statement.

---
+++ basics/decisions/simple_decision/if_elif_else.py
@@ -0,0 +1,15 @@
+# prompt user's to pick a direction
+print("pick a direction to move the paint brush")
+
+# read user's response
+direction = (input())
+
+# check if user's response is a direction
+if direction == "up":
+    print("I am painting in the upward direction!")
+elif  direction == "down":
+    print("I am painting in the downward direction!")
+elif direction == "left":
+    print("i am painting in the left direction")
+elif direction == "right":
+    print("I am painting in the right dirction")
---
+++ basics/decisions/simple_decision/modulo_operator.py
@@ -0,0 +1,10 @@
+# ask user to enter a number
+print("write in a whole number")
+number =int(input())
+
+if number % 2 == 0:
+    print("thats an even number")
+else:
+    print("its an odd number")
+
+
