dd6674b1c31aa0b752b50aff9077a84756ab7ab3
Uthman Lawal <5lawau84@solent.ac.uk>
2021-10-08 15:23:38 +0100

    Added code to demonstrate the use of an if...else statement.

---
+++ basics/decisions/simple_decision/if_else.py
@@ -0,0 +1,13 @@
+# ask user to enter activity
+print("what is the activity")
+
+# read user's response
+activity = (input())
+
+#check if user's response is calculate
+if activity == "calculate":
+    print("Performing calculations...\n")
+
+print("Activity completed!")
+
+
