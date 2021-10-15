fe86ebde3aced50db5c0245fab55942b28d64866
Uthman Lawal <5lawau84@solent.ac.uk>
2021-10-15 15:26:14 +0100

    Added code to demonstrate the use of a user-defined function with a nested decision.

---
+++ basics/functions/function_with_nesting.py
@@ -0,0 +1,9 @@
+def identify_movement():
+    print("enter a word representing what you see")
+    identify = input()
+    if input == "a large boulder":
+        print("its time to run")
+    else :
+        print("we will be fine")
+
+identify_movement()
