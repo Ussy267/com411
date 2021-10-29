28fdecbef8d420be3c33f61ce4cb807b793c8203
Uthman Lawal <5lawau84@solent.ac.uk>
2021-10-29 14:31:23 +0100

    Added code to demonstrate a simple list.

---
+++ simple_list.py
@@ -0,0 +1,11 @@
+def direction():
+    direction = ["move foward", "move backward", "Turn right", "Turn left"]
+    return direction
+
+def run():
+    print(direction())
+
+
+
+if __name__ == "__main__":
+    run()
